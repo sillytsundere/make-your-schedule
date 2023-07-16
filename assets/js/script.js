44c446aedd588c662011ae2ed25dfd71c5f809cc
Paige <paigehcarroll@gmail.com>
2023-07-15 20:52:53 -0500

    wrote code to display date at top of page, updated that code to include day suffix

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,15 +9,48 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+  //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+  //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  //
+  // DONE: Add code to display the current date in the header of the page.
+  var theMonth = dayjs().format('dddd, MMM');
+  var theYear = dayjs().format('YYYY');
+  var theDay = dayjs().format('DD');
+  console.log(theDay);
+  var nth = function(d) {
+    if (d > 3 && d < 21) return 'th';
+    switch (d % 10) {
+      case 1:  return "st";
+      case 2:  return "nd";
+      case 3:  return "rd";
+      default: return "th";
+    }
+  }
+  console.log(theDay);
+  $('#currentDay').text(theMonth + ' ' + theDay + nth(theDay) + ' ' + theYear);
 });
+
+//below function does that nth() does but with vanilla Js if else statement
+//var dateSuffix = function() {
+//  if (theDay === 1) {
+//    return theDay + 'st';
+//  }
+//  if (theDay === 2) {
+//    return theDay + 'nd';
+//  }
+//  if (theDay === 3) {
+//    return theDay + 'rd';
+//  } else {
+//    return theDay + 'th';
+//  }
+//}
