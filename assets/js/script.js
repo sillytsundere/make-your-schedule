c25768e1a567cd1dbf74b4677d7323953b7c2f83
Paige <paigehcarroll@gmail.com>
2023-07-16 19:10:57 -0500

    added html divs for each hour of the day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+var theMonth = dayjs().format('dddd, MMM');
+var theDay = dayjs().format('DD');
+var theYear = dayjs().format('YYYY');
+var currentHour = 11//dayjs().format('HH');
+console.log(currentHour);
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -8,11 +15,15 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  var saveBtn = document.querySelectorAll('.saveBtn');
-  var scheduleItem = document.querySelector('.description').innerHTML;
-  var timeBlock = document.querySelector('.time-block');
-  saveBtn.addEventListener('click', function() {
-    window.localStorage.getItem()
+  //
+  $('.saveBtn').on('click', function(event) {
+    event.preventDefault();
+    console.log('event listener save button');
+    console.log(event.target, 'event.target');
+    var item = $(event.target).parent('button').siblings('textArea').val();
+    //console.log(item); //returns the same al line 24
+    localStorage.setItem('task', item);
+    console.log(localStorage.getItem('task'), 'the inner text of the task');
   })
   //
   //
@@ -21,6 +32,17 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  $('.time-block').each(function() {
+    var hourBlock = $(this).attr('id').split('-')[1];
+    console.log(hourBlock);
+    if (hourBlock < currentHour) {
+      $(this).addClass('past');
+    } else if (hourBlock === currentHour) {
+      $(this).addClass('present');
+    } else {
+      $(this).addClass('future');
+    }}
+  )
   //
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
@@ -29,8 +51,6 @@ $(function () {
   //
   //
   // DONE: Add code to display the current date in the header of the page.
-  var theMonth = dayjs().format('dddd, MMM');
-  var theDay = dayjs().format('DD');
   var nth = function(d) {
     if (d > 3 && d < 21) return 'th';
     switch (d % 10) {
@@ -38,7 +58,6 @@ $(function () {
       case 2:  return "nd";
       case 3:  return "rd";
       default: return "th";
-    }
-  }
-  $('#currentDay').text(theMonth + ' ' + theDay + nth(theDay));
+    }}
+  $('#currentDay').text(theMonth + ' ' + theDay + nth(theDay) + ' ' + theYear);
 });
